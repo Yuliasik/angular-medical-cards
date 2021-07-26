67bc35e70fd176b3bed65f7875e5c6b64ea3e4b7
yulia_04 <yulichka.dudij@gmail.com>
2021-07-26 20:17:39 +0300

    repair for deploy 11

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/dudii-medical-cards-ng'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join('index.html'));
+  res.sendFile(__dirname + '/src/index.html');
 });
 
 // Start the app by listening on the default Heroku port
